6df6e80e9235f1410b3847deaba3c3fb8f652b68
Gitthere <ichi4g@gmail.com>
2014-06-02 14:14:42 -1000

    2nd attempt to add all new changes

--- dom_quiz.js
+++ dom_quiz.js
@@ -1,18 +1,49 @@
 (function() {
-  var score=0;
-  var newscore=0;
+  var score = 0;
 
   var quiz_ques = document.getElementById('question');
-    quiz_ques.innerHTML = 'What is the capital of Hawaii?';
-    return quiz_ques;
+  var newanswer = document.getElementById('answer');
 
-    #submit.onclick = function() {
-      if (document.getElementById('answer')) == "honolulu" {
-        var newscore = newscore+1;
-        alert("Correct!")
-      } else {
-        newscore = newscore+0; 
-        alert("Sorry, the answer is Honolulu.")
-      }
+  quiz_ques.innerHTML = 'Would you like to start the quiz?';
+
+  var questionaire = [
+    {
+      quiz_ques: "What is the capital of Hawaii?",
+      newanswer: "honolulu"
+    },
+    {
+      quiz_ques: "what is the state bird?",
+      newanswer: "nene"
+    },
+    {
+      quiz_ques: "What is the Hawaiian name for Diamond Head?",
+      newanswer: "leahi"
+    },
+    {
+      quiz_ques: "What is the traditional dance of Hawaii?",
+      newanswer:"hula"
+    },
+    {
+      quiz_ques: "In what year did Hawaii become a state?",
+      newanswer: "1959"
+    }
+  ];
+
+  var submitBtn = document.getElementById('submit');
+
+  submitBtn.onclick = function() {
+    //console.log(answer.value);
+
+    for (var i = 0; i < questionaire.length; i++) {
+      var newanswer = prompt(questionaire[i].quiz_ques) 
+
+      if (newanswer === questionaire[i].newanswer) {
+        score++;
+      }    
+    }
+
+  alert("Congratulations, your score is " + score + " out of 5.");
+  
   }
+  
 })();
